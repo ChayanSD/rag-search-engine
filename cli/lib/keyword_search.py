6afe009f3415aeb82a73d0338f7be523224c17ec
Chayan <chayansd5656@gmail.com>
2026-02-05 20:49:03 +0600

    Refactor search_command to use tokenization for improved keyword matching

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,10 +1,33 @@
 from lib.search_utils import load_movies
+import string
 
-def search_command(query: str , n_results):
+def clean_text(text: str) -> str:
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    return text
+
+def tokenize_text(text: str) -> list[str]:
+    text = clean_text(text)
+    tokens = []
+    for tok in text.split():
+         if tok:
+             tokens.append(tok)
+    return tokens
+
+def has_matching_token(query_tokens , movie_tokens):
+    for query_tok in query_tokens:
+        for movie_tok in movie_tokens:
+            if query_tok in movie_tok:
+                return True
+    return False
+
+def search_command(query: str, n_results: int):
     movies = load_movies()
     res = []
+    query_tokens = tokenize_text(query)
     for movie in movies:
-        if query in movie["title"]:
+        movie_tokens = tokenize_text(movie["title"])
+        if has_matching_token(query_tokens, movie_tokens):
             res.append(movie)
         if len(res) == n_results:
             break
