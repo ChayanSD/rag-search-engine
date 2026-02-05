3fb5af9f9e6223ac6622cde8a82464167d5c83a7
Chayan <chayansd5656@gmail.com>
2026-02-06 00:05:24 +0600

    Add build command for inverted index and add stuff on cache enhance search functionality

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.keyword_search import search_command
-
+from lib.keyword_search import (search_command
+, build_command)
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
@@ -10,9 +10,15 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    build_parser = subparsers.add_parser("build", help="Build the inverted index")
+
     args = parser.parse_args()
 
     match args.command:
+        case "build":
+            print("Building inverted index...")
+            build_command()
+            print("Inverted index built successfully.")
         case "search":
             print(f"Searching for: {args.query}")
             results = search_command(args.query, 5)
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -4,6 +4,8 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 DATA_PATH = PROJECT_ROOT / "data"/ "movies.json"
 
+CACHE_PATH = PROJECT_ROOT / "cache"
+
 def load_movies() -> list[dict]:
     with open(DATA_PATH, "r") as f:
         data= json.load(f)
